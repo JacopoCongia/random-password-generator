573a4a11bcd5c2067bb6228010ab6cf8f2134a4d
jacopo.congia <jacopo.congia@gmail.com>
2023-02-16 11:21:02 +0100

    Refactored functions

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+    "O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f",
+    "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x",
+    "y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@",
+    "#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",
+    ";","<",">",".","?","/"
+];
 
-let passOneEl = document.getElementById("pass-one-el")
-let passTwoEl = document.getElementById("pass-two-el")
-let passwordOne = ""
-let passwordTwo = ""
+const passOneEl = document.getElementById("pass-one-el");
+const passTwoEl = document.getElementById("pass-two-el");
+const btnEl = document.getElementById("btn-el");
 
-// Create function onclick for button that generates 2 passwords and stores them in 2 variables
+let passwordOne = "";
+let passwordTwo = "";
 
-function generatePassword() {
-    passwordOne = ""
-    passwordTwo = ""
+btnEl.addEventListener("click", function() {
+    passwordOne = "";
+    passwordTwo = "";
     for (i = 0; i < 15; i++) {
         let randomNumber = Math.floor(Math.random() * characters.length)
         passwordOne += characters[randomNumber]
@@ -21,8 +26,6 @@ function generatePassword() {
         passwordTwo += characters[randomNumber]
     }
     
-    passOneEl.textContent = passwordOne
-    passTwoEl.textContent = passwordTwo
-}
-
-// Create a function that randomizes 15 characters
+    passOneEl.textContent = passwordOne;
+    passTwoEl.textContent = passwordTwo;
+})
